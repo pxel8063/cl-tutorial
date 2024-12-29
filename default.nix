931fa659d25bcb87ce4237e030fdf46128d2b02c
github <83626280+pxel8063@users.noreply.github.com>
2024-12-29 21:32:41 +0900

    add trivia

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
   cl-tutorial = pkgs.callPackage ./cl-tutorial.nix {};
-  sbcl' = pkgs.sbcl.withPackages (ps: with ps; [ hunchentoot djula ]);
+  sbcl' = pkgs.sbcl.withPackages (ps: with ps; [ hunchentoot djula trivia]);
 in
 {
   inherit cl-tutorial;
