a25e9034b6bd7aa35716080808a8b7827c3974b7
github <83626280+pxel8063@users.noreply.github.com>
2024-12-29 13:17:20 +0900

    add zulu ex.openjdk and switch to 24.11

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
   cl-tutorial = pkgs.callPackage ./cl-tutorial.nix {};
   sbcl' = pkgs.sbcl.withPackages (ps: with ps; [ hunchentoot djula ]);
@@ -9,6 +9,7 @@ in
   shell = pkgs.mkShellNoCC {
     inputsFrom = [ cl-tutorial ];
     packages = [
+      pkgs.zulu23
       sbcl'
       pkgs.openssl
     ];
@@ -16,6 +17,7 @@ in
      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([
 	 pkgs.openssl ])}
    '';
-   JAVA_HOME = "/usr/local/Cellar/openjdk/23.0.1";
+   JAVA_HOME = "${pkgs.zulu23}";
+#   JAVA_HOME = "/usr/local/Cellar/openjdk/23.0.1";
  };
 }
