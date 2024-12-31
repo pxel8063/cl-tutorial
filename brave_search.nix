cd3c8033bbf156b3387c31b6e3101eb0ee549c48
github <83626280+pxel8063@users.noreply.github.com>
2024-12-31 14:40:52 +0900

    forget to include brave_serch.nix

---
+++ brave_search.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  fetchFromGitHub,
+  ...
+}:
+let
+  brave_search = pkgs.sbcl.buildASDFSystem rec {
+    pname = "brave_search";
+    version = "0.0.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "mark-watson";
+      repo = "brave_search";
+      rev = "b938a1bf0eaacfb03b6c9f856e5ce3493c4764d3";
+      hash = "sha256-QQta8J5BhH8LlabYvZNZSek8NxPLR/woik0fJyHjC64=";
+    };
+  };
+  sbcl' = pkgs.sbcl.withOverrides (self: super: {
+    inherit brave_search;
+  });
+in sbcl'.pkgs.brave_search
