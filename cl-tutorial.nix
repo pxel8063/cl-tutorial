e1ba61f966d7f11492149c2a15e884bd795b1e18
github <83626280+pxel8063@users.noreply.github.com>
2025-01-03 13:42:36 +0900

    Working with local files

--- cl-tutorial.nix
+++ cl-tutorial.nix
@@ -1,10 +1,34 @@
 {
-  pkgs
+  pkgs,
+  lib
 }:
+let
+  fs = lib.fileset;
+  sourceFiles =
+    fs.difference
+      (fs.gitTracked ./.)
+      (fs.unions [
+        (fs.maybeMissing ./result)
+        (fs.maybeMissing ./result-2)
+        ./.direnv
+        ./.envrc
+        ./.git
+        ./.gitignore
+        ./README.org
+        ./brave_search.nix
+        ./cl-tutorial.nix
+        ./default.nix
+        ./myutils.nix
+        ./shell.nix
+      ]);
+in
 pkgs.sbcl.buildASDFSystem {
   pname = "cl-tutorial";
   version = "0.0.1";
-  src = builtins.path { path = ./.; name = "cl-tutorial"; };
+  src = fs.toSource {
+    root = ./.;
+    fileset = sourceFiles;
+  };
   lispLibs = [
     pkgs.sbclPackages.djula
     pkgs.sbclPackages.hunchentoot
